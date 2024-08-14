84e51f35395d43dccf826e4678ba3d6e9261b43f
Justin Zhao <justinxzhao@gmail.com>
2024-08-14 14:32:11 -0400

    Add prompts for chatbot arena ei subset mining and subsequent judging, and alpacaeval judging prompt.

--- llm_council/prompts.py
+++ llm_council/prompts.py
@@ -1,3 +1,76 @@
+# Taken from Arena Hard: https://github.com/lm-sys/arena-hard-auto/blob/main/config/judge_config.yaml
+JUDGE_CHATBOT_ARENA_EQ_SUBSET_COMPLETIONS = """Please act as an impartial judge and evaluate the quality of the responses provided by two AI assistants to the user prompt displayed below. You will be given assistant A's answer and assistant B's answer. Your job is to evaluate which assistant's answer is better.
+
+Begin your evaluation by generating your own answer to the prompt. You must provide your answers before judging any answers.
+
+When evaluating the assistants' answers, compare both assistants' answers with your answer. You must identify and correct any mistakes or inaccurate information.
+
+Then consider if the assistant's answers are helpful, relevant, and concise. Helpful means the answer correctly responds to the prompt or follows the instructions. Note when user prompt has any ambiguity or more than one interpretation, it is more helpful and appropriate to ask for clarifications or more information from the user than providing an answer based on assumptions. Relevant means all parts of the response closely connect or are appropriate to what is being asked. Concise means the response is clear and not verbose or excessive.
+
+Then consider the creativity and novelty of the assistant's answers when needed. Finally, identify any missing important information in the assistants' answers that would be beneficial to include when responding to the user prompt.
+
+After providing your explanation, you must output only one of the following choices as your final verdict with a label:
+- [[A>>B]]: The first response is significantly better.
+- [[A>B]]: The first response is slightly better.
+- [[B>A]]: The second response is slightly better.
+- [[B>>A]]: The second response is significantly better.
+
+### USER PROMPT START ###
+{context}
+### USER PROMPT END ###
+
+### ASSISTANT A RESPONSE START ###
+{first_completion}
+### ASSISTANT A RESPONSE END ###
+
+### ASSISTANT B RESPONSE START ###
+{second_completion}
+### ASSISTANT B RESPONSE END ###
+"""
+
+
+ALPACA_EVAL_EVALUATION_TEMPLATE = """<|im_start|>system
+You are a highly efficient assistant, who evaluates and selects the best large language model (LLMs) based on the quality of their responses to a given instruction. This process will be used to create a leaderboard reflecting the most accurate and human-preferred answers.
+<|im_end|>
+<|im_start|>user
+I require a leaderboard for various large language models. I'll provide you with prompts given to these models and their corresponding outputs. Your task is to assess these responses, and select the model that produces the best output from a human perspective.
+
+## Instruction
+
+{
+    "instruction": "{instruction}",
+}
+
+## Model Outputs
+
+Here are the unordered outputs from the models. Each output is associated with a specific model, identified by a unique model identifier.
+
+{
+    {
+        "model_identifier": "m",
+        "output": "{output_1}"
+    },
+    {
+        "model_identifier": "M",
+        "output": "{output_2}"
+    }
+}
+
+## Task
+
+Evaluate the models based on the quality and relevance of their outputs, and select the model that generated the best output. Answer by first providing a concise explanation and then end your answer by providing the model identifier of the best output. We will use the last character of your output `output[-1]` as the name of the best model, so make sure you finish with the token of the model identifiers and nothing else: `m` or `M` (no quotes, no dots, no backticks, no new lines, ...). For example:
+
+### Concise explanation
+...some text...
+
+### Which is best, m or M?
+M
+
+Now is your turn.
+
+## Your answer: "Concise explanation" followed by "Which is best, m or M?"
+<|im_end|>"""
+
 GENERATE_EXPANSION = """This an example of an interesting interpersonal conflict.
 
 "I was truthful when admitting to my wife that I found her unattractive after her surgery.
@@ -316,6 +389,10 @@ PROMPT_REGISTRY = {
     "judge_conversational_dilemma_responses_sxs_granular_with_tie": JUDGE_CONVERSATIONAL_DILEMMA_RESPONSE_SXS_GRANULAR_WITH_TIE_OPTION,
     # Qualitative Reasoning Extraction.
     "qualitative_reasoning_extraction": QUALITATIVE_REASONING_EXTRACTION_TEMPLATE,
+    # Alpaca Eval.
+    "alpaca_eval_evaluation": ALPACA_EVAL_EVALUATION_TEMPLATE,
+    # Chatbot arena EQ subset judging.
+    "judge_chatbot_arena_eq_subset_completions": JUDGE_CHATBOT_ARENA_EQ_SUBSET_COMPLETIONS,
 }
 
 
