a2617d40f8531175ee38eba841a47716305b5d9e
Justin Zhao <justinxzhao@gmail.com>
2025-02-02 00:12:56 -0500

    Rename base_service to base_provider, and add a registration mechanis

--- llm_council/processors/services/base_provider.py
+++ llm_council/processors/services/base_provider.py
@@ -6,24 +6,50 @@ A Service defines a specification that is used by the Processor to manage large 
 
 The Council Service consists of multiple LLMs, which are attended by their own Service.
 """
+
 import dotenv
 import os
+from functools import wraps
 
-from llm_council.processors.services.utils import get_model_name, get_provider_name, reset_file
+from llm_council.processors.services.utils import (
+    get_model_name,
+    get_provider_name,
+    reset_file,
+)
 
-
+# Load environment variables.
 dotenv.load_dotenv()
 
 
-class BaseService:
+# Global registry to store services
+PROVIDER_REGISTRY = {}
+
+
+def provider(provider_name: str, api_key_name: str):
+    """Class decorator to register service classes with metadata."""
+
+    def decorator(cls):
+        # Store class reference and metadata in the registry
+        enabled = os.getenv(api_key_name) is not None
+        PROVIDER_REGISTRY[provider_name] = {
+            "class": cls,
+            "api_key_name": api_key_name,
+            "enabled": enabled,
+        }
+
+        return cls  # Return the class unchanged
+
+    return decorator
+
+
+class BaseProvider:
 
     def __init__(self, llm: str) -> None:
         self.llm = llm
         self.model_name = get_model_name(llm)
         self.provider_name = get_provider_name(llm)
-        pass
 
-    def __api_key(self) -> str | None: 
+    def __api_key(self) -> str | None:
         raise NotImplementedError
 
     def request_url(self) -> str:
@@ -98,3 +124,7 @@ class BaseService:
     def reset_requests_and_responses(self, outdir: str):
         reset_file(self.get_requests_path(outdir))
         reset_file(self.get_responses_path(outdir))
+
+    async def get_async_completion_task(self, prompt, temperature, schema_name):
+        # raise NotImplementedError
+        pass
