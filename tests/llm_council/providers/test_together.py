df8d72bf25db6965cf78c46b5ed4baa904615e1f
Justin Zhao <justinxzhao@gmail.com>
2025-02-19 22:49:42 -0500

    Update together unit test.

--- tests/llm_council/providers/test_together.py
+++ tests/llm_council/providers/test_together.py
@@ -1,55 +1,70 @@
 from llm_council.providers.together_provider import TogetherProvider
 import asyncio
+import tqdm.asyncio
 from llm_council.structured_outputs import STRUCTURED_OUTPUT_REGISTRY
-from tqdm.asyncio import tqdm
 
 
 def test_async_client():
-    """Sample code that tests openai async client."""
+    """Sample code that tests TogetherProvider async client."""
     provider = TogetherProvider(
         "together://meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
     )
 
-    async def get_all_tasks() -> tuple[list, list]:
-        tasks = [provider.get_async_completion_task("Say Hello.") for _ in range(3)]
-        extractions = []
-        completions = []
-        for result in tqdm.as_completed(tasks, total=len(tasks)):
-            extraction, completion = await result
-            extractions.append(extraction)
-            completions.append(completion)
-        return extractions, completions
-
-    extractions, completions = asyncio.run(get_all_tasks())
-
-    assert len(extractions) == 3
-    assert len(completions) == 3
-
-
-def test_async_client_structured_output():
-    """Sample code that tests openai async client with structured outputs."""
-    provider = TogetherProvider(
-        "together://meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
-    )
-
-    async def get_all_tasks() -> tuple[list, list]:
+    async def get_all_tasks() -> list:
         tasks = [
             provider.get_async_completion_task(
-                "Create a hypothetical user.", schema_name="test_user"
+                "Say Hello.",
+                task_metadata={
+                    "llm": "together://meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
+                },
             )
             for _ in range(3)
         ]
 
-        extractions = []
-        completions = []
-        for result in tqdm.as_completed(tasks, total=len(tasks)):
-            extraction, completion = await result
-            extractions.append(extraction)
-            completions.append(completion)
-        return extractions, completions
+        completion_texts = []
+        for future in tqdm.asyncio.tqdm.as_completed(tasks, total=len(tasks)):
+            result = await future
+            task_metadata = result["task_metadata"]
+            llm = task_metadata["llm"]
+            completion_text = result["completion_text"]
+            completion_texts.append(completion_text)
+        return completion_texts
 
-    extractions, completions = asyncio.run(get_all_tasks())
+    completion_texts = asyncio.run(get_all_tasks())
 
-    assert len(extractions) == 3
-    assert isinstance(extractions[0], STRUCTURED_OUTPUT_REGISTRY["test_user"])
-    assert len(completions) == 3
+    assert len(completion_texts) == 3
+
+
+def test_async_client_structured_output():
+    """Sample code that tests TogetherProvider async client with structured outputs."""
+    provider = TogetherProvider(
+        "together://meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
+    )
+
+    async def get_all_tasks() -> list:
+        tasks = [
+            provider.get_async_completion_task(
+                "Create a hypothetical user.",
+                schema_class_path="llm_council.structured_outputs.User",
+                task_metadata={
+                    "llm": "together://meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
+                },
+            )
+            for _ in range(3)
+        ]
+
+        structured_outputs = []
+        for future in tqdm.asyncio.tqdm.as_completed(tasks, total=len(tasks)):
+            result = await future
+            task_metadata = result["task_metadata"]
+            llm = task_metadata["llm"]
+            structured_output = result["structured_output"]
+            structured_outputs.append(structured_output)
+
+        return structured_outputs
+
+    structured_outputs = asyncio.run(get_all_tasks())
+
+    assert len(structured_outputs) == 3
+    # First item of the list, and the first item of the tuple is the actual structured output completion.
+    assert structured_outputs[0].__class__.__name__ == "User"
